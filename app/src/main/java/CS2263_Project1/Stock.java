f3f9cdfa7d1ad011f9b4cac8522055e0503c30a5
David Hellwig <helldavi@isu.edu>
2021-11-03 21:13:18 -0600

    Added tile and stock classes

---
+++ app/src/main/java/CS2263_Project1/Stock.java
@@ -0,0 +1,16 @@
+package CS2263_Project1;
+
+public class Stock {
+    private int value;
+
+    private String company;
+
+
+    public String getCompany(){
+        return company;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
