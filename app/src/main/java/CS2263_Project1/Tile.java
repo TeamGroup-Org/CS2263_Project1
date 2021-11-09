12705d0e51e04a996c15abaffceeadd985ebed1f
David Hellwig <helldavi@isu.edu>
2021-11-08 21:20:51 -0700

    test

--- app/src/main/java/CS2263_Project1/Tile.java
+++ app/src/main/java/CS2263_Project1/Tile.java
@@ -1,14 +1,24 @@
 package CS2263_Project1;
 
+/**
+ * @author David Hellwig
+ */
+
 public class Tile {
     boolean isSafe = false;
 
     boolean isSpent = false;
 
+    /**
+     * Sets safe status to true, indicating is safe
+     */
     public void setSafeStatus(){
         isSafe = true;
     }
 
+    /**
+     * Sets status to true, indicating that it is placed
+     */
     public void setSpentStatus(){
         isSpent = true;
     }
