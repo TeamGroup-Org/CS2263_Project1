28ad4031241f5bc57fec5bd485a497cba7ac930d
barlekow <barlekow@isu.edu>
2021-11-09 20:09:59 -0700

    This is for the TileTray updated

--- app/src/main/java/CS2263_Project1/TileTray.java
+++ app/src/main/java/CS2263_Project1/TileTray.java
@@ -1,16 +1,20 @@
 package CS2263_Project1;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class TileTray {
     private ArrayList<Tile> tray;
 
-    public ArrayList<Tile> getTray() {
-        return this.tray;
+    public TileTray(ArrayList<CS2263_Project1.Tile> tray) {
+        this.tray = tray;
     }
 
-    public void addTray(Tile tray) {
-        boolean add = this.tray.add(tray);
+    public ArrayList<CS2263_Project1.Tile> getTray() {
+        return tray;
+    }
+
+    public void setTray(ArrayList<CS2263_Project1.Tile> tray) {
+        this.tray = tray;
     }
 }
