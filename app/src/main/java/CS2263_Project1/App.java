4a0904712468390f9e1a5008330c6343a5de053b
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2021-11-12 11:30:52 -0700

    Begin process of absorbing corporation tray into board to keep board-relevant objects encapsulated

--- app/src/main/java/CS2263_Project1/App.java
+++ app/src/main/java/CS2263_Project1/App.java
@@ -3,6 +3,6 @@ package CS2263_Project1;
 public class App {
     public static void main(String[] args) {
         Board board = new Board();
-        CorporationTray corporationTray = new CorporationTray();
+
     }
 }
--- app/src/main/java/CS2263_Project1/Board.java
+++ app/src/main/java/CS2263_Project1/Board.java
@@ -1,5 +1,7 @@
 package CS2263_Project1;
 
+import java.util.ArrayList;
+
 /**
  * @author Noah Owens
  */
@@ -8,8 +10,10 @@ public class Board {
     public boolean gameCanEnd;
     private String[] boardArray = new String[108];
     private Tile[] tileArray = new Tile[108];
+    private ArrayList<Corporation> corporationTray = new ArrayList<>();
 
     public Board() {
+
         initBoard();
     }
 
@@ -36,6 +40,22 @@ public class Board {
 
         //set gameCanEnd to false
         gameCanEnd = false;
+
+        //create the corporations (should be replaced by a json load I think.)
+        initCorporationTray();
+    }
+
+    /**
+     * initCorporationTray() is used at the start of the game to make all 7 hotel chains and place them into an ArrayList
+     */
+    private void initCorporationTray() {
+        corporationTray.add(new Corporation(1, "Worldwide", false, false, 0));
+        corporationTray.add(new Corporation(2, "Sackson", false, false, 0));
+        corporationTray.add(new Corporation(3, "Festival", false, false, 0));
+        corporationTray.add(new Corporation(4, "Imperial", false, false, 0));
+        corporationTray.add(new Corporation(5, "American", false, false, 0));
+        corporationTray.add(new Corporation(6, "Continental", false, false, 0));
+        corporationTray.add(new Corporation(7, "Tower", false, false, 0));
     }
 
     /**
