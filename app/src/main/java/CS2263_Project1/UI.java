f28a399d2a6438ce2d29ad1a9cdf4d8208221a37
Coby Garner <cobyone12345@gmail.com>
2021-11-12 13:38:51 -0700

    added UI class and module-info.java for javafx runtime components

---
+++ app/src/main/java/CS2263_Project1/UI.java
@@ -0,0 +1,129 @@
+package CS2263_Project1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class UI extends Application {
+
+    public Pane makeGameBoard() {
+        char[] charArray = new char[] {'A','B','C','D','E','F','G','H','I'};
+        GridPane pane = new GridPane();
+        pane.setGridLinesVisible(true);
+        for (int i = 1; i<13; i++){
+            for (int j = 0; j< 9; j++) {
+                Button tile = new Button();
+                tile.setText(String.valueOf(i) + charArray[j]);
+                tile.setPrefWidth(40);
+                pane.add(tile,i,j);
+            }
+        }
+        return pane;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setMaxWidth(500); primaryStage.setMaxHeight(500);
+        primaryStage.setTitle("Acquire");
+
+        //Buttons
+        Button startButton = new Button(); startButton.setWrapText(true);
+        Button loadButton = new Button(); loadButton.setWrapText(true);
+        Button quitButton = new Button(); quitButton.setWrapText(true);
+
+        startButton.setText("New Game");
+        loadButton.setText("Load Game");
+        quitButton.setText("Exit to desktop");
+
+        Label label = new Label("Acquire");label.setFont(Font.font(40));
+        Pane pane = new Pane(label, startButton, loadButton, quitButton);
+        Scene scene1 = new Scene(pane, 500, 500);
+
+        label.setLayoutY(120);label.setLayoutX(175);
+        startButton.setLayoutX(150); startButton.setLayoutY(200);
+        loadButton.setLayoutX(225); loadButton.setLayoutY(200);
+        quitButton.setLayoutX(175); quitButton.setLayoutY(225);
+
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+
+        quitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                primaryStage.close();
+            }
+        });
+
+        loadButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                primaryStage.close();
+                Stage acquireStage = new Stage();
+                acquireStage.setTitle("Acquire");
+                Scene scene2 = new Scene(makeGameBoard(), 200, 200);
+                acquireStage.setScene(scene2);
+                acquireStage.show();
+            }
+        });
+
+    }
+}
+
+
+//    public Corporation getCorporation(){
+//
+//    }
+//
+//    public void mergePlayerChoice(){
+//
+//    }
+//
+//    public int sellPlayerStock(Player player){
+//        System.out.print("How much would you like to sell? ~");
+//        String sellAmount = scanner.next();
+//        return player.sellStock();
+
+
+//    }
+//
+//    public int buyPlayerStock(Player player){
+//        System.out.print("How much would you like to buy? ~");
+//        System.in.
+//        return player.buyStock();
+//    }
+//
+//    public void startTurn(Player player){
+//        //What would you like to do?
+//        //buttons for trade, hold, sell
+//        //System.in...
+//        //if (choice == trade){
+//        // player.trade();}
+//        //else if (choice == hold){
+//        //player.hold();}
+//        //else if (choice == sell){
+//        //player.sellStock();}
+//    }
+//
+//    public void endTurn(Player player){
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module CS2263.Project1.app.main {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens CS2263_Project1;
+}
