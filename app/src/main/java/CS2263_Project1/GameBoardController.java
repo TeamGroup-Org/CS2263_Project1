6f241b699f40799d154f4d58382f5ce1b9cc2154
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2021-12-05 00:09:34 -0700

    First player is determined by the tile closest to A1 at game start

--- app/src/main/java/CS2263_Project1/GameBoardController.java
+++ app/src/main/java/CS2263_Project1/GameBoardController.java
@@ -57,14 +57,136 @@ public class GameBoardController {
      * Behaviors in the initialize method will be run when gameBoard.fxml is loaded
      */
     @FXML public void initialize() {
+        // Load in all fields
         initGridPane();
-        clearSystemPane();
         updateMoney();
         updatePlayerTiles();
+
+        // Set up first player's turn
+        determineStartingPlayer();
+        setPlayerTurn();
     }
 
     /**
-     * Updates board relevant features (Money totals, tile colors, stock totals)
+     * The closest player to A1 becomes the first person to take their turn.
+     *
+     * @throws IndexOutOfBoundsException if a tile doesn't have a 3 character long id
+     */
+    public void determineStartingPlayer() throws IndexOutOfBoundsException {
+        Tile p1Closest = playClosest(player1);
+        Tile p2Closest = playClosest(player2);
+        String p1String = "";
+        String p2String = "";
+        int p1Int;
+        int p2Int;
+
+        clearSystemPane();
+        updateSpentOnly();
+
+        if (p1Closest.id.charAt(0) < p2Closest.id.charAt(0)) {
+            playerTurn = 1;
+            infoLabel.setText("Player 1 goes first with tile " + p1Closest.id);
+        }
+        else if (p1Closest.id.charAt(0) == p2Closest.id.charAt(0)) {
+            try {
+                p1String = String.valueOf(p1Closest.id.charAt(1)) + String.valueOf(p1Closest.id.charAt(2));
+            }
+            catch (IndexOutOfBoundsException e) {
+                p1String = String.valueOf(p1Closest.id.charAt(1));
+            }
+            finally {
+                p1Int = Integer.parseInt(p1String);
+            }
+
+            try {
+                p2String = String.valueOf(p2Closest.id.charAt(1)) + String.valueOf(p2Closest.id.charAt(2));
+            }
+            catch (IndexOutOfBoundsException e) {
+                p2String = String.valueOf(p2Closest.id.charAt(1));
+            }
+            finally {
+                p2Int = Integer.parseInt(p2String);
+            }
+
+            if (p1Int < p2Int) {
+                playerTurn = 1;
+            }
+        }
+        else {
+            playerTurn = 2;
+            infoLabel.setText("Player 2 goes first with tile " + p2Closest.id);
+        }
+    }
+
+    /**
+     * Find the tile closest to A1 in a given player's hand and plays it to the board
+     * Letter proximity is prioritized (B9 is closer than A2)
+     *
+     * @throws IndexOutOfBoundsException if a tile doesn't have a 3 character long id
+     */
+    public Tile playClosest(Player p) throws IndexOutOfBoundsException {
+        Tile currentClosest = new Tile("Z12", false, null, null, null, null, null, null);
+        Tile spentTile;
+        String idString;
+        String currentClosestIdString;
+        int idNum;
+        int currentClosestIdNum;
+        int i = 0;
+
+        // Iterate through player 1 hand and find the tile closest to A1
+        for (Tile t : p.getHand()) {
+            i++;
+            // If the letter is closer to A, that becomes the current closest
+            if (t.id.charAt(0) < currentClosest.id.charAt(0)) {
+                currentClosest = t;
+            }
+
+            // If the letters are tied, then consider numbers
+            else if (t.id.charAt(0) == currentClosest.id.charAt(0)) {
+                try {
+                    idString = String.valueOf(t.id.charAt(1)) + String.valueOf(t.id.charAt(2));
+                }
+                catch (IndexOutOfBoundsException e) {
+                    idString = String.valueOf(t.id.charAt(1));
+                }
+
+                try {
+                    currentClosestIdString = String.valueOf(currentClosest.id.charAt(1)) + String.valueOf(currentClosest.id.charAt(2));
+                }
+                catch (IndexOutOfBoundsException e) {
+                    currentClosestIdString = String.valueOf(currentClosest.id.charAt(1));
+                }
+
+                idNum = Integer.parseInt(idString);
+                currentClosestIdNum = Integer.parseInt(currentClosestIdString);
+
+                if (idNum < currentClosestIdNum) {
+                    currentClosest = t;
+                }
+            }
+        }
+
+        // Plays closest tile from hand and draws again
+        log.info("Closest tile for player" + p.playerIdentity + " is " + currentClosest.nullCheckedToString());
+
+        for (int j = 0; j < p.getHand().size(); j++) {
+            Tile tile = p.getHand().get(j);
+            if (tile == currentClosest) {
+                p.playTile(gameBoard, tile);
+                tile.setOwner(p);
+
+                p.getTileFromHand(j);
+                drawTile(p);
+
+                spentTileStorage.add(tile);
+            }
+        }
+        updatePlayerTiles();
+        return currentClosest;
+    }
+
+    /**
+     * Updates the colors of the most recent tile played
      */
     public void updateBoard() {
         int mostRecentTile = spentTileStorage.size() - 1;
@@ -75,7 +197,6 @@ public class GameBoardController {
             }
         };
 
-        // Match the most recently played tile with it's complement gridPane label and get access to that Node
         for (Node n : gridPane.getChildren()) {
             if (n instanceof Label) {
                 if (((Label) n).getText() == spentTileStorage.get(mostRecentTile).id) {
@@ -86,6 +207,21 @@ public class GameBoardController {
 
         if (spentTileStorage.get(mostRecentTile).isSpent) {
             colorTargetLabel.setStyle("-fx-background-color: #808080;");
+        }
+    }
+
+    /**
+     * Ensures that all tiles which are on in spentTileStorage recieve a spent background color
+     */
+    public void updateSpentOnly() {
+        for (int i = 0; i < spentTileStorage.size(); i++) {
+            for (Node n : gridPane.getChildren()) {
+                if (n instanceof Label) {
+                    if (((Label) n).getText() == spentTileStorage.get(i).id) {
+                        n.setStyle("-fx-background-color: #808080;");
+                    }
+                }
+            }
         }
     }
 
@@ -123,7 +259,6 @@ public class GameBoardController {
                 tileArrayTracker++;
             }
         }
-
         log.info("Grid Pane labels created and centered");
     }
 
@@ -142,11 +277,10 @@ public class GameBoardController {
      * Stores Actual Tiles into the buttons to be referenced later using getUserId() Method
      */
     public void updatePlayerTiles(){
-
+        // Nightmarishly long and awful way to do what coby did with a loop, but the loop broke things
+        // So now it's hardcoded
         switch (playerTurn) {
-            case 1: // Nightmarishly long and awful way to do what coby did with the loop
-                    // But I think the loop was causing problems in edge cases and the hardcoded one is easier to read.
-                    // I'm leaving the loop in, just commented out because I think it's nice and we can go back to it later
+            case 1:
                     tile1.setText(player1.getHand().get(0).id);
                     tile1.setUserData(player1.getHand().get(0));
 
@@ -186,29 +320,6 @@ public class GameBoardController {
                     break;
             default: break;
         }
-
-//        ObservableList<Node> buttonList = TileHolder.getChildren();
-//
-//        if (playerTurn == 1) {
-//            int i = 0;
-//            for(Node n : buttonList)
-//                if (n instanceof Button) {
-//                    ((Button) n).setText(player1.getHand().get(i).id);
-//                    n.setUserData(player1.getHand().get(i));
-//                    i++;
-//                }
-//        }
-//        else if (playerTurn == 2) {
-//            int i = 0;
-//            for(Node n : buttonList)
-//                if(n instanceof Button) {
-//                    ((Button) n).setText(player2.getHand().get(i).id);
-//                    n.setUserData(player2.getHand().get(i));
-//                    i++;
-//                }
-//            }
-
-        log.info("TileHolder filled with P" + playerTurn + " hand");
     }
 
     public void placeTile(ActionEvent e){
@@ -307,6 +418,9 @@ public class GameBoardController {
         updateBoard();
     }
 
+    /**
+     * Removes text from info and alert labels to give the next player a clean slate.
+     */
     public void clearSystemPane() {
         for (Node n : systemPane.getChildren()) {
             if (n instanceof Label) {
